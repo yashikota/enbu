9ef0b88a10122995693b64d9e5a47cf914cf5b77
yashikota <52403688+yashikota@users.noreply.github.com>
2026-06-26 17:27:19 +0000

    set workflow template download timeout

--- internal/cli/init.go
+++ internal/cli/init.go
@@ -261,7 +261,10 @@ func downloadEnbuSyncWorkflow(ctx context.Context) ([]byte, error) {
 		return nil, err
 	}
 
-	resp, err := http.DefaultClient.Do(req)
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
